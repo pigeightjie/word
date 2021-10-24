c2713671a6ce93fa0409d7f9e4436db496ad3e66
pigeightjie <91340479+pigeightjie@users.noreply.github.com>
2021-10-24 16:11:55 +0800

    1

---
+++ 未命名2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int j,k,l;
+for(j=1;j<5;j++)
+{
+	for(k=1;k<5;k++)
+	{
+		for(l=1;l<5;l++)
+		{
+		if(j!=k&&j!=l&&k!=l)
+		{
+			printf("%d %d %d\n",j,k,l);
+		}	
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
+
+	
+	
+} 
+	
+	
